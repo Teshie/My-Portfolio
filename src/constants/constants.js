c2fb61dc894fbe7dcf508439b8837cf968c36ec9
Teshome Ayechiluhem <teshome@ayechiluhem>
2021-08-04 21:23:39 +0300

    change project descriptions on projects section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,7 @@
 export const projects = [
   {
     title: "MERN Memories",
-    description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    description: "Hello",
     image: "/images/1.png",
     tags: ["Mongo", "Express", "React", "Node"],
     source:
@@ -14,7 +13,7 @@ export const projects = [
     title: "E-Commerce",
     description:
       "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio.",
-    image: "/images/2.png",
+    image: "/images/1.png",
     tags: ["React", "JavaScript"],
     source: "https://google.com",
     visit: "https://google.com",
